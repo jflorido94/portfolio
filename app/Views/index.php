4c0a255c3b4b545a508f2649dec19189d257de32
jflorido94 <hbranky22@hotmail.com>
2022-06-28 16:35:54 +0200

    test 0.006 css

--- app/Views/index.php
+++ app/Views/index.php
@@ -28,7 +28,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
 
   <!-- Personal CSS -->
-  <link href="<?php echo base_url('assets/css/styles.css') ?>" rel="stylesheet" />
+  <!-- <link href="<?php echo base_url('assets/css/styles.css') ?>" rel="stylesheet" /> -->
 
   <!-- Font Awesome CSS 5.15.4 -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
