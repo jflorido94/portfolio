16d2b54a1936a013b1817d9fc9f7882a2a0cd85e
jflorido94 <hbranky22@hotmail.com>
2022-06-29 19:23:33 +0200

    test 0.008 bg

--- app/Views/index.php
+++ app/Views/index.php
@@ -58,7 +58,7 @@
         <img src="assets/img/profile-img2.jpg" alt="" class="img-fluid rounded-circle">
         <h1 class="text-light"><a href="index.html">Javier Florido</a></h1>
         <div class="social-links mt-3 text-center">
-          <a href="#" class="linkedin"><i class="fas fa-linkedin-in    "></i></a>
+          <a href="#" class="linkedin"><i class="fas fa-linkedin"></i></a>
         </div>
       </div>
 
@@ -66,7 +66,7 @@
         <ul>
           <li><a href="#hero" class="nav-link scrollto active"><i class="fas fa-home"></i> <span>Inicio</span></a></li>
           <li><a href="#about" class="nav-link scrollto"><i class="fas fa-user-alt"></i> <span>Sobre mi</span></a></li>
-          <li><a href="#facts" class="nav-link scrollto"> <i class="fas fa-award"></i> <span>Logros</span></a></li>
+          <li><a href="#facts" class="nav-link scrollto"><i class="fas fa-award"></i> <span>Logros</span></a></li>
           <li><a href="#skills" class="nav-link scrollto"><i class="fas fa-book-open"></i> <span>Habilidades</span></a></li>
           <li><a href="#resume" class="nav-link scrollto"><i class="fas fa-route"></i> <span>Experiencia</span></a></li>
           <li><a href="#portfolio" class="nav-link scrollto"><i class="fas fa-briefcase"></i> <span>Portfolio</span></a></li>
