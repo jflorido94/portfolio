776dbab2bd2ccb2351df129ea919458b897b6cd3
jflorido94 <hbranky22@hotmail.com>
2022-07-04 20:40:32 +0200

    test 0.0087 mail

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,8 +35,8 @@ $routes->setAutoRoute(false);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
-$routes->get('contact', 'Contact::index');
+$routes->get('/', 'Contact::index');
+// $routes->get('contact', 'Contact::index');
 
 
 /*
